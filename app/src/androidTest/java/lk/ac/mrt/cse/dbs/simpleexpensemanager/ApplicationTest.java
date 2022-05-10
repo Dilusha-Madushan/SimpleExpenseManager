345668cbd1bcce6d62306b036c6564037b75ca9c
Dilusha-Madushan <dilushamadushan9912@gmail.com>
2022-05-10 15:50:45 +0530

    Add test methods

--- app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
+++ app/src/androidTest/java/lk/ac/mrt/cse/dbs/simpleexpensemanager/ApplicationTest.java
@@ -17,13 +17,99 @@
 package lk.ac.mrt.cse.dbs.simpleexpensemanager;
 
 import android.app.Application;
+import android.content.Context;
 import android.test.ApplicationTestCase;
+
+import androidx.test.core.app.ApplicationProvider;
+
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import java.nio.charset.Charset;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.List;
+import java.util.Random;
+
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.ExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.control.PersistentExpenseManager;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.exception.InvalidAccountException;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.ExpenseType;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.data.model.Transaction;
+import lk.ac.mrt.cse.dbs.simpleexpensemanager.ui.MainActivity;
+
+import static org.junit.Assert.assertThrows;
+import static org.junit.Assert.assertTrue;
 
 /**
  * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
  */
-public class ApplicationTest extends ApplicationTestCase<Application> {
-    public ApplicationTest() {
-        super(Application.class);
+
+public class ApplicationTest {
+
+    private ExpenseManager expenseManager;
+
+    @Before
+    public void DBSetup(){
+        expenseManager = new PersistentExpenseManager(ApplicationProvider.getApplicationContext());
     }
+
+    @Test
+    public void addAccountTest(){
+        String accountNo = "12345";
+        String bankName = "ABC";
+        String accountHolderName = "David";
+        double balance = 1000;
+
+        expenseManager.addAccount(accountNo , bankName , accountHolderName , balance);
+
+        List<String> accountNumList = expenseManager.getAccountNumbersList();
+
+        assertTrue(accountNumList.contains(accountNo));
+    }
+
+    @Test
+    public void addTransactionTest1() throws InvalidAccountException {
+        int day = 1;
+        int month = 1;
+        int year = 2022;
+        String accountNo = "12345";
+        ExpenseType expenseType = ExpenseType.INCOME;
+        String amount = "500";
+
+        Calendar calendar = Calendar.getInstance();
+        calendar.set(year, month, day);
+        Date transactionDate = calendar.getTime();
+
+        expenseManager.updateAccountBalance(accountNo , day , month , year , expenseType , amount);
+
+        Transaction testTransaction = new Transaction(transactionDate , accountNo , expenseType , Double.parseDouble(amount));
+
+        List<Transaction> transactions = expenseManager.getTransactionLogs();
+
+        assertTrue(transactions.contains(testTransaction));
+    }
+
+//    @Test
+//    public void addTransactionTest2() {
+//        int day = 1;
+//        int month = 1;
+//        int year = 2022;
+//        String accountNo = "11111";
+//        ExpenseType expenseType = ExpenseType.INCOME;
+//        String amount = "500";
+//
+//        List<String> accountNumList = expenseManager.getAccountNumbersList();
+//
+//        while(accountNumList.contains(accountNo)){
+//            byte[] array = new byte[5]; // length is bounded by 7
+//            new Random().nextBytes(array);
+//            accountNo = new String(array, Charset.forName("UTF-8"));
+//        }
+//
+//        String finalAccountNo = accountNo;
+//        assertThrows(InvalidAccountException.class,  expenseManager.updateAccountBalance(finalAccountNo, day , month , year , expenseType , amount));
+//
+//    }
 }
